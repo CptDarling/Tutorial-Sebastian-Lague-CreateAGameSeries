11ca9d697bca07525669c832fb916c1657c035cb
Richard Holyoak <richard.holyoak@jjsmanufacturing.com>
2021-11-28 09:58:13 +0000

    E09. obstacle placement

--- Assets/Scripts/Common/Utility.cs
+++ Assets/Scripts/Common/Utility.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Utility
@@ -8,6 +8,27 @@ namespace Utility
     {
         public static string Enemy { get { return "Enemy"; } private set { } }
         public static string Player { get { return "Player"; } private set { } }
+    }
+
+    public static class Array
+    {
+
+        // Fisher–Yates shuffle
+        public static T[] ShuffleArray<T>(T[] array, int seed)
+        {
+            System.Random prng = new System.Random(seed);
+
+            // Loop through all elements of the array except the last one.
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int randomIndex = prng.Next(i, array.Length);
+                T orig = array[i];
+                array[i] = array[randomIndex];
+                array[randomIndex] = orig;
+            }
+
+            return array;
+        }
     }
 
 }
--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -6,10 +6,15 @@ public class MapGenerator : MonoBehaviour
 {
 
     [SerializeField] Transform tilePrefab;
+    [SerializeField] Transform obstaclePrefab;
     [SerializeField] Vector2 mapSize;
 
     [Range(0, 1)]
     [SerializeField] float outlinePercent;
+
+    List<Coord> allTileCoords;
+    Queue<Coord> shuffledTileCoords;
+    [SerializeField] int seed = 1;
 
     private void Start()
     {
@@ -18,6 +23,11 @@ public class MapGenerator : MonoBehaviour
 
     public void GenerateMap()
     {
+
+        allTileCoords = new List<Coord>();
+
+        Vector3 localScale = Vector3.one * (1 - outlinePercent);
+
         string holderName = "Generated Map";
         Transform mapHolder = transform.Find(holderName);
         if (mapHolder)
@@ -28,18 +38,53 @@ public class MapGenerator : MonoBehaviour
         mapHolder = new GameObject(holderName).transform;
         mapHolder.parent = transform;
 
-        float tileWidth = tilePrefab.GetComponent<Renderer>().bounds.size.x;
-        float tileHeight = tilePrefab.GetComponent<Renderer>().bounds.size.z;
-
         for (int x = 0; x < mapSize.x; x++)
-        {
             for (int y = 0; y < mapSize.y; y++)
             {
-                Vector3 tilePosition = new Vector3((-mapSize.x + tileWidth) / 2f + (float)x * tileWidth, 0, (-mapSize.y + tileHeight) / 2f + (float)y * tileHeight);
+                allTileCoords.Add(new Coord(x, y));
+
+                Vector3 tilePosition = CoordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, tilePrefab.transform.rotation);
-                newTile.localScale = Vector3.one * (1 - outlinePercent);
+                newTile.localScale = localScale;
                 newTile.parent = mapHolder;
             }
+
+        // Shuffle the tile coords
+        shuffledTileCoords = new Queue<Coord>(Utility.Array.ShuffleArray(allTileCoords.ToArray(), seed));
+
+        int obstacleCount = 10;
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            Coord coord = GetRandomCoord();
+            Vector3 position = CoordToPosition(coord.x, coord.y);
+            Transform newObstacle = Instantiate(obstaclePrefab, position + Vector3.up * 0.5f, obstaclePrefab.transform.rotation);
+            newObstacle.SetParent(mapHolder);
+        }
+
+    }
+
+    Vector3 CoordToPosition(int x, int y)
+    {
+        float tileWidth = tilePrefab.GetComponent<Renderer>().bounds.size.x;
+        float tileHeight = tilePrefab.GetComponent<Renderer>().bounds.size.z;
+        return new Vector3((-mapSize.x + tileWidth) / 2f + (float)x * tileWidth, 0, (-mapSize.y + tileHeight) / 2f + (float)y * tileHeight);
+    }
+
+    Coord GetRandomCoord()
+    {
+        Coord randomCoord = shuffledTileCoords.Dequeue();
+        shuffledTileCoords.Enqueue(randomCoord);
+        return randomCoord;
+    }
+
+    public struct Coord
+    {
+        public int x;
+        public int y;
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
         }
     }
 
